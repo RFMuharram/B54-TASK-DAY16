48cdc89addeab9507a7cfb18b9af6c22c0dc683e
Muharram <muharram@MacBook-Air-Rizky.local>
2024-05-04 05:15:49 +0700

    seventh commit

--- hello.js
+++ hello.js
@@ -5,6 +5,7 @@ const path = require ("path")
 const config = require("./config/config.json")
 const {Sequelize, QueryTypes} = require("sequelize")
 const sequelize = new Sequelize(config.development)
+const blogModel = require("./models").tb_blog;
 
 app.set('view engine', 'hbs');
 app.set("views", path.join(__dirname, "./pages"))
@@ -21,6 +22,8 @@ app.get("/addProjectBootstrap", project)
 app.post("/addProjectBootstrap", addProject)
 app.get("/testi", testi)
 app.get("/backpageBootstrap", contact)
+app.get("/project/:id", blogDetail)
+
 
 // const data = []
 
@@ -28,40 +31,39 @@ app.get("/backpageBootstrap", contact)
 async function home(req,res) {
     const query ="SELECT * FROM tb_blogs";
     const data = await sequelize.query(query, { type: QueryTypes.SELECT });
+    console.log("data ", data)
     res.render("index", {data : data})  
 }
 function project(req,res) {
     res.render("addProjectBootstrap")  
 }
-function deletePost(req,res) {
+async function deletePost(req,res) {
     const {id} = req.params; 
-    data.splice(id,1)
-    
+    const query = `DELETE FROM tb_blogs WHERE id=${id}`;
+    const data = await sequelize.query(query, { type: QueryTypes.DELETE });
+
     res.redirect("/")
 }
-function blogEdit(req,res) {
+async function blogEdit(req,res) {
     const {id} = req.params;
+    const data = await blogModel.findOne({
+        where: { id },
+      });
 
-    const selectedData = data[id];
-    selectedData.id = id
-
-    res.render("blog-Edit", {data : selectedData})  
+    res.render("blog-Edit", {data})  
 }
-function editedBlog(req,res) {
+async function editedBlog(req,res) {
     const {tittle, content, id} = req.body
+    const query  = `UPDATE public.tb_blogs SET tittle='${tittle}', content='${content}' WHERE id=${id}`;
+    const data = await sequelize.query(query, {type: QueryTypes.UPDATE});
     
-    data[id] = {
-        tittle,
-        content 
-    }
     res.redirect("/")
 }
-function addProject(req,res) {
+async function addProject(req,res) {
     const {tittle, content} = req.body
-    data.unshift({
-        tittle,
-        content,
-    });
+    const query = `INSERT INTO tb_blogs (tittle,content,image,"createdAt","updatedAt") VALUES('${tittle}','${content}','https://images.pexels.com/photos/2670898/pexels-photo-2670898.jpeg?auto=compress&cs=tinysrgb&w=600', now(), now())`;
+    const data = await sequelize.query(query, { type: QueryTypes.INSERT });
+
     res.redirect("/")
 }
 function testi(req,res) {
@@ -70,7 +72,16 @@ function testi(req,res) {
 function contact(req,res) {
     res.render("backpageBootstrap")  
 }
+async function blogDetail(req,res) {
+    const {id} = req.params;
 
+    const query =`SELECT * FROM tb_blogs WHERE id=${id}`;
+    const data = await sequelize.query(query, { type: QueryTypes.SELECT });
+
+    console.log("dapat", data[0])
+
+    res.render("project",{data:data[0]}); 
+}
 app.listen (port,()=>{
     console.log("server is running on port:" , port)
 })
